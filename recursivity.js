206e3910d20f55ce2fe543dffacb11b347622c1a
Rihab Naouar <rihabbennaouar@gmail.com>
2024-06-27 19:57:08 +0100

    modified:   recursivity.algo modified:   recursivity.js

--- recursivity.js
+++ recursivity.js
@@ -9,7 +9,7 @@ function Leap(year) {
                 return `${year} is not a leap`;
             } 
         }else{
-            return `${year} is not a leap`;
+            return `${year} is a leap`;
         }
         return `${year} is a leap`;
     }else {
@@ -43,3 +43,22 @@ function Fibonacci(n) {
     }
 return result;
 }
+//Power Function
+    function power(x, n) {
+        let result
+        if (n === 0) {
+            result = 1;
+        }
+        if (n === 1) {
+            result = x;
+        }
+        else if (n < 0) {
+            result =  x* 1 / power(x, -n);
+        }else {
+            result = x * power(x, n - 1);
+        }
+        return result
+    }
+    let x = 3;
+    let n = 2;
+    console.log(power(x, n))
